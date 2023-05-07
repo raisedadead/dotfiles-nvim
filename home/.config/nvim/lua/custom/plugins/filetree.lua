7c6781e1f5393144c69e6e26c89e94e0ff4ef084
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-05-07 23:31:07 +0530

    fix: update filetree configs

--- home/.config/nvim/lua/custom/plugins/filetree.lua
+++ home/.config/nvim/lua/custom/plugins/filetree.lua
@@ -10,6 +10,9 @@ return {
   },
   config = function ()
     require('neo-tree').setup {
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
       filesystem = {
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
