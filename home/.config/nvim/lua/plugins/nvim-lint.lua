23cd020a16908ce48a61d475e6376a7b69b3a0f8
Mrugesh Mohapatra <noreply@mrugesh.dev>
2025-08-13 22:24:58 +0530

    fix(lint): disable markdown

---
+++ home/.config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/lua/plugins/lint.lua
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      -- If Markdown linting exists, remove it
+      if opts.linters_by_ft then
+        opts.linters_by_ft.markdown = nil
+      end
+    end,
+  },
+}
