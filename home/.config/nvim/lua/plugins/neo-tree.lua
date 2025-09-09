909f09f56be64f55d52eda9b96752004a587e7cb
Mrugesh Mohapatra <noreply@mrugesh.dev>
2025-09-09 11:18:43 +0530

    fix(neotree): disable auto open and enable close last

--- home/.config/nvim/lua/plugins/neo-tree.lua
+++ home/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,7 +2,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+    auto_clean_after_session_restore = true,
+    close_if_last_window = true,
     filesystem = {
+      hijack_netrw_behavior = "disabled",
       filtered_items = {
         visible = true,
         show_hidden_count = true,
@@ -17,4 +21,9 @@ return {
       },
     },
   },
+  -- do not open netrw and neotree on startup
+  init = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
